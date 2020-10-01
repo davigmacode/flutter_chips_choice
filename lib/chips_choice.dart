82e2d92ae3edded3a19993e9399a04819858bd56
Noam Liran <noam@sightfull.com>
2020-10-01 03:19:35 +0300

    Fixed a bug preventing custom margin from being set and added support for setting materialTapTargetSize for chips

--- lib/chips_choice.dart
+++ lib/chips_choice.dart
@@ -1,5 +1,6 @@
 library chips_choice;
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart' show FilterChip, ThemeData, Theme, Brightness;
 
@@ -195,7 +196,7 @@ class _ChipsChoiceItem<T> extends StatelessWidget {
       : const Color(0x00000000);
 
     return Padding(
-      padding: config.margin ?? isWrapped ? const EdgeInsets.all(0) : const EdgeInsets.all(5),
+      padding: config.margin ?? (isWrapped ? const EdgeInsets.all(0) : const EdgeInsets.all(5)),
       child: FilterChip(
         label: Text(
           item.label,
@@ -214,6 +215,7 @@ class _ChipsChoiceItem<T> extends StatelessWidget {
         selectedColor: selectedBackgroundColor,
         checkmarkColor: checkmarkColor,
         showCheckmark: config.showCheckmark == true,
+        materialTapTargetSize: config.materialTapTargetSize,
         selected: selected,
         onSelected: item.disabled == false
           ? (_selected) => onSelect(_selected)
@@ -302,6 +304,9 @@ class ChipsChoiceItemConfig {
   /// whether the chips use checkmark or not
   final bool showCheckmark;
 
+  /// Configures the minimum size of the tap target.
+  final MaterialTapTargetSize materialTapTargetSize;
+
   /// label style
   final TextStyle labelStyle;
 
@@ -339,6 +344,7 @@ class ChipsChoiceItemConfig {
     this.elevation = 0,
     this.pressElevation = 0,
     this.showCheckmark = true,
+    this.materialTapTargetSize,
     this.labelStyle = const TextStyle(),
     this.selectedColor,
     this.unselectedColor,
