8f58b04e64ae54a63f1d027c06c39cd430394de7
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-09-22 10:54:05 +0700

    Use new way to define type

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -3,13 +3,13 @@ import 'package:flutter/widgets.dart';
 import 'choice_item.dart';
 
 /// Callback when the value changed
-typedef void C2Changed<T>(T value);
+typedef C2Changed<T> = void Function(T value);
 
 /// Callback to load the choice items
-typedef Future<List<C2Choice<T>>> C2ChoiceLoader<T>();
+typedef C2ChoiceLoader<T> = Future<List<C2Choice<T>>> Function();
 
 /// Builder for custom choice item
-typedef Widget? C2Builder<T>(C2Choice<T> item);
+typedef C2Builder<T> = Widget? Function(C2Choice<T> item);
 
 /// alias to AsyncMemoizer
 class C2ChoiceMemoizer<T> extends AsyncMemoizer<List<C2Choice<T>>> {}
